246afae0d798fd6445154455267236597360fae5
Aurel1701 <gantzfan96@gmail.com>
2022-04-11 17:04:26 +0200

    edit

--- Snack2/assets/main.js
+++ Snack2/assets/main.js
@@ -1,6 +1,10 @@
 const names = ['grogu', 'stefano', 'luke', 'sara']
+const result = []
 
 for (let name of names) {
     name = name.charAt(0).toUpperCase() + name.substr(1);
     console.log(name);
+    result.push(name)
+    console.log(result);
+    
 }
